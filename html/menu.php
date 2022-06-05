3fb18a7033bf888f541ca0ddea69be6a3aa31e38
José Guilherme <jose.guilherme96@outlook.com.br>
2022-06-04 23:02:32 -0300

    Menu

---
+++ html/menu.php
@@ -0,0 +1,17 @@
+<header class="header">
+    <nav>
+        <ul class="menu">
+            <li><a href="#">Quem Somos</a></li> 
+            <li><a href="#">Quadrinhos</a></li> 
+            <li><a href="#">Desenhos</a></li>   
+            <li><a href="monica.php">Personagens</a></li> 
+            <li><a href="#">Colorir</a></li> 
+            <li><a href="historia-parte1.php">História</a></li> 
+            <li><a href="brinquedos.php">Binquedos</a></li> 
+            <li><a href="jogos.php">Jogos</a></li> 
+            <li><a href="#">Gibis</a></li> 
+            <li><a href="#">Atrações</a></li> 
+            <li><a href="#">Autor</a></li>
+        </ul>
+    </nav>
+</header>   
