9b69b29c411133b9f27d61a8f5b55937b36385ea
MelissaGodoy <melissaraineri328@gmail.com>
2022-06-09 18:29:14 -0300

    adicionando pagina

--- html/menu.php
+++ html/menu.php
@@ -10,8 +10,13 @@
             <li><a href="historia-parte1.php">História</a></li> 
             <li><a href="brinquedos.php">Binquedos</a></li> 
             <li><a href="jogos.php">Jogos</a></li> 
+<<<<<<< Updated upstream
             <li><a href="gibis.php">Gibis</a></li> 
             <li><a href="#">Atrações</a></li> 
+=======
+            <li><a href="#">Gibis</a></li> 
+            <li><a href="atracoes.php">Atrações</a></li> 
+>>>>>>> Stashed changes
             <li><a href="#">Autor</a></li>
         </ul>
     </nav>
