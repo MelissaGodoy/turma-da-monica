2ed00ef1232936015dcc90a809d373dc574cb4ee
joseguilherme96 <87539880+joseguilherme96@users.noreply.github.com>
2022-06-09 15:22:35 -0300

    Adicionado novas páginas

---
+++ html/gibis.php
@@ -0,0 +1,88 @@
+    <!DOCTYPE html>
+<html lang="pt-br"
+  <head>
+     <meta charset="utf-8">
+     <link href="../css/menu.css" rel="stylesheet">
+     <link href="../css/default.css" rel="stylesheet">
+     <link href="../css/csscolorir.css" rel="stylesheet" type="text/css">
+  </head>
+ <body> 
+     <?php
+        include "menu.php";
+     ?>
+        <section>
+            <h2> Gibi 1</h2>
+            <img src="../img/Gibi1verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
+                at tellus dictum, eu porttitor nunc pellentesque.
+            </p>
+            <h2>Gibi 2</h2>
+            <img src="../img/Gibi2verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
+                 at tellus dictum, eu porttitor nunc pellentesque.
+            </p> 
+            <h2>Gibi 3</h2>
+            <img src="../img/Gibi3verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
+                at tellus dictum, eu porttitor nunc pellentesque.
+            </p>
+            <h2>Gibi 4</h2>
+            <img src="../img/Gibi4verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
+                at tellus dictum, eu porttitor nunc pellentesque.
+            </p>
+            <h2>Gibi 5</h2>
+            <img src="../img/Gibi5verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
+                 at tellus dictum, eu porttitor nunc pellentesque.
+            </p> 
+            <h2>Gibi 6</h2>
+            <img src="../img/Gibi6verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
+                at tellus dictum, eu porttitor nunc pellentesque.
+            </p>
+            <h2>Gibi 7</h2>
+            <img src="../img/Gibi7verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
+                 at tellus dictum, eu porttitor nunc pellentesque.
+            </p> 
+            <h2>Gibi 8</h2>
+            <img src="../img/Gibi8verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
+                at tellus dictum, eu porttitor nunc pellentesque.
+            </p>
+            <h2>Gibi 9</h2>
+            <img src="../img/Gibi9verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica ">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
+                at tellus dictum, eu porttitor nunc pellentesque.
+            </p>
+        </section>  
+        <footer>
+            <img src="" alt="logo-quem-somos">
+            <h5>Quem somos</h5>
+            <img src="" alt="Logo do facebook">
+            <img src="" alt="Logo do instagram">
+            <img src="" alt="Logo do twitter">
+            <img src="" alt="Logo do orkut">
+            <img src="" alt="Turma da mônica">
+        </footer>
+ </body>
+</html> 
