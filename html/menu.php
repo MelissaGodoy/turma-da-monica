3cfbffa3289753f19bfe3b124eb412e994d1f52a
joseguilherme96 <87539880+joseguilherme96@users.noreply.github.com>
2022-06-09 15:28:48 -0300

    Adicionado nome das novas páginas no menu

--- html/menu.php
+++ html/menu.php
@@ -1,15 +1,16 @@
 <header class="header">
     <nav>
         <ul class="menu">
+            <li><a href="index.php">Home</a></li> 
             <li><a href="#">Quem Somos</a></li> 
             <li><a href="#">Quadrinhos</a></li> 
             <li><a href="#">Desenhos</a></li>   
             <li><a href="monica.php">Personagens</a></li> 
-            <li><a href="#">Colorir</a></li> 
+            <li><a href="colorir.php">Colorir</a></li> 
             <li><a href="historia-parte1.php">História</a></li> 
             <li><a href="brinquedos.php">Binquedos</a></li> 
             <li><a href="jogos.php">Jogos</a></li> 
-            <li><a href="#">Gibis</a></li> 
+            <li><a href="gibis.php">Gibis</a></li> 
             <li><a href="#">Atrações</a></li> 
             <li><a href="#">Autor</a></li>
         </ul>
