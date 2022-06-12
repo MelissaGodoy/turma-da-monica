c848e39fed28feb20a508b00e98f40dc99d68a98
José Guilherme <jose.guilherme96@outlook.com.br>
2022-06-12 01:02:09 -0300

    Revisão

--- html/menu.php
+++ html/menu.php
@@ -6,9 +6,9 @@
             <li><a href="quadrinhos.php">Quadrinhos</a></li> 
             <li><a href="desenhos.php">Desenhos</a></li>   
             <li><a href="personagens.php">Personagens</a></li>
-            <li><a href="jogos.php">Colorir</a></li> 
+            <li><a href="colorir.php">Colorir</a></li> 
             <li><a href="historia-parte1.php">História</a></li> 
-            <li><a href="atracoes.php">Brinquedos</a></li>
+            <li><a href="brinquedos.php">Brinquedos</a></li>
             <li><a href="jogos.php">Jogos</a></li> 
             <li><a href="gibis.php">Gibis</a></li> 
             <li><a href="atracoes.php">Atrações</a></li>
