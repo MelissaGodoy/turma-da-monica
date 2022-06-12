23b36cba6d7546692b187ce503eb508e322e2fd1
MelissaGodoy <melissaraineri328@gmail.com>
2022-06-12 00:22:04 -0300

    link css

--- html/personagens.php
+++ html/personagens.php
@@ -1,46 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>turma da monica</title>
-    <link rel="stylesheet" href="../css/personagens.css">
-</head>
-    
-<body class="bodypers">
-    <?php
-        include "menu.php";
-    ?>
-    <article>
-        <section class="primeiraimagem">
-        
-            <li><a href="monica.php"><img src="../img/monicapers.-removebg-preview.png"></a></li>
-
-        </section>
-
-        <section class="segundaimagem">
-            <li><a href="cebolinha.php"><img src="../img/cebolinhapers.-removebg-preview.png"></a></li>
-        </section>
-
-        <section class="terceiraimagem">
-            <li><a href="magali.php"><img src="../img/magalipers.-removebg-preview.png"></a></li>
-        </section>
-
-        <section class="quartaimagem">
-            <li><a href="cascao.php"><img src="../img/cascaopers.-removebg-preview.png"></a></li>
-        </section>
-
-        <footer class="footerpers">
-            <nav>
-                <ul class="primeirosbotoes">
-                    <li><a href="monica.php">monica</a></li>
-                    <li><a href="cebolinha.php">Cebolinha</a></li> 
-                    <li><a href="magali.php">Magali</a></li>
-                    <li><a href="cascao.php">Cascão</a></li>    
-                </ul>
-            </nav>
-        </footer>
-
-    </article>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>turma da monica</title>
+        <link href="../css/default.css" rel="stylesheet">
+        <link href="../css/menu.css" rel="stylesheet">
+        <link rel="stylesheet" href="../css/personagens.css">
+    </head>
+    <body class="bodypers">
+        <?php
+            include "menu.php";
+        ?>
+        <article>
+            <section class="primeiraimagem">
+                <li><a href="monica.php"><img src="../img/monicapers.-removebg-preview.png"></a></li>
+            </section>
+            <section class="segundaimagem">
+                <li><a href="cebolinha.php"><img src="../img/cebolinhapers.-removebg-preview.png"></a></li>
+            </section>
+            <section class="terceiraimagem">
+                <li><a href="magali.php"><img src="../img/magalipers.-removebg-preview.png"></a></li>
+            </section>
+            <section class="quartaimagem">
+                <li><a href="cascao.php"><img src="../img/cascaopers.-removebg-preview.png"></a></li>
+            </section>
+            <footer class="footerpers">
+                <nav>
+                    <ul class="primeirosbotoes">
+                        <li><a href="monica.php">monica</a></li>
+                        <li><a href="cebolinha.php">Cebolinha</a></li> 
+                        <li><a href="magali.php">Magali</a></li>
+                        <li><a href="cascao.php">Cascão</a></li>    
+                    </ul>
+                </nav>
+            </footer>
+        </article>
+    </body>
+</html>
