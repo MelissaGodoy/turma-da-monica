0a2438384b1c9e0156154a0e6c01dd0f14f29bc1
José Guilherme <jose.guilherme96@outlook.com.br>
2022-06-11 21:35:10 -0300

    Revisão

--- html/desenhos.php
+++ html/desenhos.php
@@ -11,10 +11,13 @@
         <h1 class="th">DESENHOS</h1>
         <article>
             <h4 class="desenho">Maratona com a turminha</h4>
+            <img class="img10" src="../img/desenho5.jpg" height=500px height="600px">
             <a href="https://www.youtube.com/watch?v=dTEDhPwmxKw" target="_blank"><h3 class="q1">A lei de Murphy</h3></a>
             <h4 class="des">Maratona em casa com a turma</h4>
+            <img class="de2" src="../img/desenhho2.png">
             <a href="https://www.youtube.com/watch?v=AUebXlvQ8Bg" target="_blank"><h3 class="q2">A lei de Murphy</h3></a>
             <h4 class="de">Maratona dos episodios mais vistos</h4>
+            <img class="de3" src="../img/desenho3.png">
             <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
             <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
             <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
@@ -22,14 +25,9 @@
             <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
         </article>
         <section>
-            <img class="img10" src="desenho5.jpg" height=500px height=600px">
-        </section>
-                <img class="img6" src="../img/quadrinho1.jpg">
-                <img class="img7" src="../img/quadrinho2.jpg">
-                <img class="img8" src="../img/quadrinho3.jpg">
-                <img class="img9" src="../img/quadrinho1.jpg"> 
-                <img="" alt="aqui temos o quadrinho da monica">
-                <img class="de2" src="../img/desenhho2.png">
-                <img class="de3" src="../img/desenho3.png">
+            
+            
+            
+        </section> 
     </body>
 </html>
--- html/quadrinhos.php
+++ html/quadrinhos.php
@@ -1,49 +1,23 @@
 <!doctype html>
 <html lang="pt-br">
-    <head><meta charset="utf-8">
-    <title>quadrinhos</title></head>
-    <link rel="stylesheet" href="yteste.css">
-    <body class="body1">
-        <header class="cabeçalho">
-           
-            <header class="header">
-                <nav>
-                    <ul class="menu">
-                        <li><a href="#">Quem Somos</a></li> 
-                        <li><a href="#">Quadrinhos</a></li> 
-                        <li><a href="#">Desenhos</a></li>   
-                        <li><a href="#">Personagens</a></li> 
-                        <li><a href="#">Colorir</a></li> 
-                        <li><a href="#">História</a></li> 
-                        <li><a href="#">Binquedos</a></li> 
-                        <li><a href="#">Jogos</a></li> 
-                        <li><a href="#">Gibis</a></li> 
-                        <li><a href="#">Atrações</a></li> 
-                        <li><a href="#">Autor</a></li>
-                    </ul>
-                </nav>
-            </header>
-            <h1 class="th">QUADRINHOS</h1>
-            
-        </header>
-        <article >
-        
-    </article>
-
-    <section>
-
-        <a href="https://youtu.be/ghNYCIGHoRs"><img class="img3" src="quadrinho1.jpg"/></a>
-        <a href="https://crianca.mppr.mp.br/arquivos/File/publi/turma_da_monica/monica_saude_bucal.pdf">  <img class="img4" src="quadrinho3.jpg"/></a>
-        <img class="img5" src="quadrinho2.jpg"/>
-    </section>
-     
-
-
-
-
-<footer class="ms">
-   
-
-
+    <head>
+        <meta charset="utf-8">
+        <link href="../css/default.css" rel="stylesheet">
+        <link href="../css/menu.css" rel="stylesheet">
+        <link rel="stylesheet" href="yteste.css">
+        <title>quadrinhos</title>
+    </head>
+    <body>
+        <?php include "menu.php" ?>
+        <h1 class="th">QUADRINHOS</h1>
+        <section>
+            <a href="https://youtu.be/ghNYCIGHoRs"><img class="img3" src="../img/quadrinho1.jpg"/></a>
+            <a href="https://crianca.mppr.mp.br/arquivos/File/publi/turma_da_monica/monica_saude_bucal.pdf"> 
+            <img class="img6" src="../img/quadrinho1.jpg">
+            </a>
+            <img class="img7" src="../img/quadrinho2.jpg">
+            <img class="img8" src="../img/quadrinho3.jpg">
+            <img class="img9" src="../img/quadrinho1.jpg">
+        </section>
     </body>
 </html>
