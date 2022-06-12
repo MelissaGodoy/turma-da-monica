946fa6bd2a6c99da57d4d35b470ace2614320b07
José Guilherme <jose.guilherme96@outlook.com.br>
2022-06-12 01:04:05 -0300

    Revisão

--- html/atracoes.php
+++ html/atracoes.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="../css/default.css" rel="stylesheet">
+        <link href="../css/menu.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
