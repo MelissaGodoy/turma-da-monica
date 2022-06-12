169023a9b1198035afb1beceb6a5007742fc567d
José Guilherme <jose.guilherme96@outlook.com.br>
2022-06-11 21:02:55 -0300

    Revisão

--- html/desenhos.php
+++ html/desenhos.php
@@ -1,58 +1,35 @@
 <!DOCTYPE html>
-<!-- saved from url=(0045)file:///F:/YASMIN%20CERTO%20PRJ/desenhos.html -->
-<html lang="pt-br"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>DESENHOS</title><link rel="stylesheet" href="yteste.css"></head>
-    
-           <body class="body1">
-            <header class="cabeçalho">
-               
-                <header class="header">
-                    <nav>
-                        <ul class="menu">
-                            <li><a href="#">Quem Somos</a></li> 
-                            <li><a href="#">Quadrinhos</a></li> 
-                            <li><a href="#">Desenhos</a></li>   
-                            <li><a href="#">Personagens</a></li> 
-                            <li><a href="#">Colorir</a></li> 
-                            <li><a href="#">História</a></li> 
-                            <li><a href="#">Binquedos</a></li> 
-                            <li><a href="#">Jogos</a></li> 
-                            <li><a href="#">Gibis</a></li> 
-                            <li><a href="#">Atrações</a></li> 
-                            <li><a href="#">Autor</a></li>
-                        </ul>
-                    </nav>
-                </header>
-            <h1 class="th">DESENHOS</h1>
-            <article>
-                <h4 class="desenho">Maratona com a turminha</h4>
-                <a href="https://www.youtube.com/watch?v=dTEDhPwmxKw" target="_blank"><h3 class="q1">A lei de Murphy</h3></a>
-
-                <h4 class="des">Maratona em casa com a turma</h4>
-                <a href="https://www.youtube.com/watch?v=AUebXlvQ8Bg" target="_blank"><h3 class="q2">A lei de Murphy</h3></a>
-                <h4 class="de">Maratona dos episodios mais vistos</h4>
-
-                <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
-                <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
-                <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
-                <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
-                <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
-                
-                </article>
-                <section><img class="img10" src="desenho5.jpg height=500px height=600px"></section>
-                 
-
-   
-                <img class="img6" src="./desenhos_files/quadrinho1.jpg">
-                <img class="img7" src="./desenhos_files/quadrinho2.jpg">
-                <img class="img8" src="./desenhos_files/quadrinho3.jpg">
-                <img class="img9" src="./desenhos_files/quadrinho1.jpg"> 
+<html lang="pt-br">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>DESENHOS</title>
+        <link href="../css/default.css" rel="stylesheet">
+        <link href="../css/menu.css" rel="stylesheet">
+        <link rel="stylesheet" href="yteste.css"></head>
+    <body class="body1">
+        <?php include "menu.php"; ?>
+        <h1 class="th">DESENHOS</h1>
+        <article>
+            <h4 class="desenho">Maratona com a turminha</h4>
+            <a href="https://www.youtube.com/watch?v=dTEDhPwmxKw" target="_blank"><h3 class="q1">A lei de Murphy</h3></a>
+            <h4 class="des">Maratona em casa com a turma</h4>
+            <a href="https://www.youtube.com/watch?v=AUebXlvQ8Bg" target="_blank"><h3 class="q2">A lei de Murphy</h3></a>
+            <h4 class="de">Maratona dos episodios mais vistos</h4>
+            <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
+            <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
+            <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
+            <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
+            <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
+        </article>
+        <section>
+            <img class="img10" src="desenho5.jpg" height=500px height=600px">
+        </section>
+                <img class="img6" src="../img/quadrinho1.jpg">
+                <img class="img7" src="../img/quadrinho2.jpg">
+                <img class="img8" src="../img/quadrinho3.jpg">
+                <img class="img9" src="../img/quadrinho1.jpg"> 
                 <img="" alt="aqui temos o quadrinho da monica">
-
-                <img class="de2" src="desenhho2.png">
-                <img class="de3" src="desenho3.png">
-
-
-                <footer><a href="quem somos.html"> <img src="quemsomos.webp.crdownload" alt="aqui temos a imagem de todo mundo" class="ms"> </a>
-                </footer>
-        </body></html>
+                <img class="de2" src="../img/desenhho2.png">
+                <img class="de3" src="../img/desenho3.png">
+    </body>
+</html>
--- html/faleconosco.php
+++ html/faleconosco.php
@@ -1,31 +1,23 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fale conosco</title>
-    <link rel="stylesheet" href="yteste.css"> 
-</head>
-<body class="fc">
-
-    <?php
-    include "menu.php";
-    
-    ?>
-    <section>
-    <h1 class="f1">Fale conosco</h1>
-    <h2 class="f2" >Nome:</h2>
-    <input class="f4" type="search"> 
-    <h3 class="f3">Mensagem:</h3>
-    <input class="f5" type="search"> 
-    <input class="f6" type="button" value="enviar">
-    </section>
-
-
-    <footer>
-
-    </footer>
-
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="../css/default.css" rel="stylesheet">
+        <link href="../css/menu.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Fale conosco</title>
+        <link rel="stylesheet" href="yteste.css"> 
+    </head>
+    <body class="fc">
+        <?php include "menu.php";?>
+        <section>
+            <h1 class="f1">Fale conosco</h1>
+            <h2 class="f2" >Nome:</h2>
+            <input class="f4" type="search"> 
+            <h3 class="f3">Mensagem:</h3>
+            <input class="f5" type="search"> 
+            <input class="f6" type="button" value="enviar">
+        </section>
+    </body>
 </html>
