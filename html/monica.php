ef3ea1f038fe124fc86903d6323ce2cae80481db
MelissaGodoy <melissaraineri328@gmail.com>
2022-06-12 23:55:59 -0300

    atualizando

--- html/monica.php
+++ html/monica.php
@@ -1,33 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>turma da monica</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link  rel="stylesheet" href="../css/monica.css">
-</head>
-<body class="body">
-        <?php
-            include "menu.php";
-        ?>
-    <article class="article">
-
-        <aside class="img-maior">
-            <img src="../img/monica-removebg-preview.png" alt="monica com samsao">   
-        </aside>
-
-        <aside class="img-menor">
-            <img src="../img/8b7da58601dc75e668cdfdb5d81139b4.png" alt="turma da monica">   
-        </aside>
-
-        <section class="monica">
-            <h1>Mônica</h1>
-        </section>
-
-        <section class="texto">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam velit metus, condimentum non lacinia ac, fringilla eget ex. Aenean mattis ipsum at ligula
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>turma da monica</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <link  rel="stylesheet" href="../css/monica.css">
+    </head>
+    <body class="body">
+            <?php
+                include "menu.php";
+            ?>
+        <h1 class="monica">Mônica</h1>
+        <article class="article">
+            <aside class="img-maior">
+                <img src="../img/monica-removebg-preview.png" alt="monica com samsao">   
+            </aside>
+            <aside class="img-menor">
+                <img src="../img/8b7da58601dc75e668cdfdb5d81139b4.png" alt="turma da monica">   
+            </aside>
+            <p class="texto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam velit metus, condimentum non lacinia ac, fringilla eget ex. Aenean mattis ipsum at ligula
                 condimentum iaculis. Mauris laoreet, metus eget pharetra luctus, ipsum lacus placerat leo, quis dapibus leo tellus ac leo. Sed pulvinar porttitor nisl vel 
                 sollicitudin. Fusce in semper nisl. Pellentesque at ornare diam, semper commodo enim. Cras eu mollis metus, eu suscipit eros. Nulla vel dolor non odio semper
                 porta nec nec nisi. Donec vitae egestas quam, vitae tristique magna. Sed enim dui, cursus et justo iaculis, auctor venenatis dui. Mauris scelerisque consequat 
@@ -37,21 +30,18 @@
                 sollicitudin. Fusce in semper nisl. Pellentesque at ornare diam, semper commodo enim. Cras eu mollis metus, eu suscipit eros. Nulla vel dolor non odio semper
                 porta nec nec nisi. Donec vitae egestas quam, vitae tristique magna. Sed enim dui, cursus et justo iaculis, auctor venenatis dui. Mauris scelerisque consequat 
                 risus, eget condimentum leo tincidunt vel. Morbi semper ligula lobortis libero tempus, non gravida diam vestibulum.
-                sollicitudin. </p>
-         
-        </section>
-
-        <footer class="final">
-            <nav>
-                <ul class="footer">
-                    <li><a href="cebolinha.php">Cebolinha</a></li> 
-                    <li><a href="magali.php">Magali</a></li> 
-                    <li><a href="cascao.php">Cascão</a></li>    
-                </ul>
-            </nav>
-        </footer>
-      
-</article>
-</body>
+                sollicitudin. 
+            </p>
+            <footer class="final">
+                <nav>
+                    <ul class="footer">
+                        <li><a href="cebolinha.php">Cebolinha</a></li> 
+                        <li><a href="magali.php">Magali</a></li> 
+                        <li><a href="cascao.php">Cascão</a></li>    
+                    </ul>
+                </nav>
+            </footer>
+        </article>
+    </body>
 </html>
 
