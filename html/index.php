64216bbcea7f386595f10f40ea73623674c2d239
Giliarde0 <106849333+Giliarde0@users.noreply.github.com>
2022-06-14 20:16:15 -0300

    Validado pela W3C

--- html/index.php
+++ html/index.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
+        <title> index </title>
         <meta charset="utf-8">
-     <link rel="stylesheet" href="">
-     <link href="../css/menu.css" rel="stylesheet">
-     <link href="../css/default.css" rel="stylesheet">
-     <link href="../css/index.css" rel="stylesheet">
+        <link href="../css/menu.css" rel="stylesheet">
+        <link href="../css/default.css" rel="stylesheet">
+        <link href="../css/index.css" rel="stylesheet">
     </head>
     <body>
-    <?php include "menu.php"; ?>
+        <?php
+        include "menu.php";
+        ?>
     <section>
         <h2> Mônica </h2>
         <img src="../img/rostodamonica.png" alt="Está opção direciona diretamente para a pagina da personagem Mônica, aonde você descobre um pouco mais sobre a personagem, e nesta imagem vemos">
@@ -20,70 +22,24 @@
         <img src="../img/rostodocascao.png" alt="Está opção direciona diretamente para a pagina do personagem Cascão, aonde você descobre um pouco mais sobre o personagem, e nesta imagem vemos">
         <h2> Atrações </h2>
         <img src="../img/turmadamonicashow.png" alt="Está opção direciona para a pagina Atrações, onde há informações de localização, preços e quais atrações temos disponíveis para visitar  e nesta imagem vemos">
-        <p>
-           
-        </p> 
         <h2> Joguinhos </h2>
-        <img src="../img/controle2.png" alt="Está opção direciona para a pagina Joguinhos, onde há diversos jogos para o visitante se divertir e nesta imagem vemos">
-        <p>
-
-        </p>
+        <img src="../img/controle2.png" alt="Está opção direciona para a pagina Joguinhos, onde há diversos jogos para o visitante se divertir e nesta imagem vemos">    
         <h2> Gibis </h2>
         <img src="../img/gibisturmadamonica.png" alt="Está  opção direciona para a pagina Gibis, aonde ele pode encontrar vários Gibis para comprar  e nesta imagem vemos">
-        <p>
-           
-        </p> 
         <h2> Quadrinhos </h2>
-        <img src="../img/logo quadrinhos.jpg " alt="Está  opção direciona o visitante do site para a pagina Quadrinhos, aonde ele pode encontrar quadrinhos da Turma da Monica  e nesta imagem vemos">
-        <p>
-           
-        </p> 
+        <img src="../img/logo quadrinhos.jpg" alt="Está s   opção direciona o visitante do site para a pagina Quadrinhos, aonde ele pode encontrar quadrinhos da Turma da Monica  e nesta imagem vemos">
         <h2> Brinquedos </h2>
         <img src="../img/brinquedos.png" alt="Está opção direciona para a pagina Brinquedos, aonde você encontra diversos brinquedos da Turma da Monica para comprar e nesta imagem vemos">
-        <p>
-           
-        </p> 
         <h2> Desenhos</h2>
         <img src="../img/desenhos.png" alt="Está opção direciona para a pagina Desenhos, aonde você encontra os episódios animados da Turma da Monica e nesta imagem vemos">
-        <p>
-           
-        </p> 
         <h2> Historia </h2>
         <img src="../img/historia.png" alt="Está opção direciona para a pagina Historia, aonde você encontra a historia contando um pouco sobre a Turma da Monica e nesta imagem vemos ">
-        <p>
-           
-        </p> 
         <h2> Autor </h2>
         <img src="../img/autor.png " alt="Está opção direciona  para a pagina Autor, aonde você encontra informações sobre o Mauricio de Sousa, criador da Turma da Monica e neste ícone vemos uma foto do Mauricio de Sousa.">
-        <p>
-           
-        </p> 
         <h2> Personagens </h2>
         <img src="../img/personagens.png" alt="Está opção direciona para a pagina Personagens, um espaço especial para contar um pouco da historia dos personagens da Turma da Monica e nesta imagem vemos">
-        <p>
-           
-        </p> 
         <h2> Colorir</h2>
         <img src="../img/colorir.png" alt="Está opção direciona para a pagina Colorir, aonde você encontra desenhos para colorir da Turma da Monica e nesta imagem vemos">
-        <p>
-           
-        </p> 
     </section>            
     </body>
-</html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 </html>
