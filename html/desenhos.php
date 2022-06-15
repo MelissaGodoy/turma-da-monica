327964f08b71ee049f28656cd50d5d2d93658954
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2022-06-15 19:27:51 -0300

    Validado w3

--- html/desenhos.php
+++ html/desenhos.php
@@ -6,28 +6,25 @@
         <link href="../css/default.css" rel="stylesheet">
         <link href="../css/menu.css" rel="stylesheet">
         <link rel="stylesheet" href="yteste.css"></head>
-    <body class="body1">
+  
         <?php include "menu.php"; ?>
-        <h1 class="th">DESENHOS</h1>
+        <body class="body1">
         <article>
+            <h1 class="th">DESENHOS</h1>
             <h4 class="desenho">Maratona com a turminha</h4>
-            <img class="img10" src="../img/desenho5.jpg" height=500px height="600px">
+            <img class="img10" src="../img/desenho5.jpg"  alt="Aqui temos a imagem do desenho da turma da Mônica">
             <a href="https://www.youtube.com/watch?v=dTEDhPwmxKw" target="_blank"><h3 class="q1">A lei de Murphy</h3></a>
             <h4 class="des">Maratona em casa com a turma</h4>
-            <img class="de2" src="../img/desenhho2.png">
+            <img class="de2" src="../img/desenhho2.png" alt="Aqui temos a imagem do desenho da turma da Mônica">
             <a href="https://www.youtube.com/watch?v=AUebXlvQ8Bg" target="_blank"><h3 class="q2">A lei de Murphy</h3></a>
-            <h4 class="de">Maratona dos episodios mais vistos</h4>
-            <img class="de3" src="../img/desenho3.png">
-            <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
-            <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
-            <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
-            <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
-            <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"><h3 class=""></h3></a>
+            <h4 class="de">Maratona dos episodios mais vistos </h4>
+            <img class="de3" src="../img/desenho3.png" alt="Aqui temos a imagem do desenho da turma da Mônica">
+            <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"></a>
+            <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"></a>
+            <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"></a>
+            <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"></a>
+            <a href="https://www.youtube.com/watch?v=UYeFDbgrETE" target="_blank"></a>
         </article>
-        <section>
-            
-            
-            
-        </section> 
+        
     </body>
 </html>
