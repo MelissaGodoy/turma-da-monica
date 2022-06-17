a6ce371bd8a2ade94a367c9c6ffc02cc00857736
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2022-06-16 23:31:09 -0300

    ok

--- html/quem-somos.php
+++ html/quem-somos.php
@@ -5,6 +5,9 @@
         <title>QUEM SOMOS</title>
         <link href="../css/default.css" rel="stylesheet">
         <link href="../css/menu.css" rel="stylesheet">
+        <link href="estilo-tabela.css" rel="stylesheet">
+        <link href="estilo.css" rel="stylesheet">
+
         <!--<link rel="stylesheet" href="yteste.css" type="text/css" />-->
     </head>
     <?php include "menu.php";?>
@@ -13,10 +16,10 @@
     <body class="body2">
         <article>
             <h1 class="th">QUEM SOMOS</h1>
-            <img src="../img/pessoa1.jpg" class="qm1" alt="aqui temos a imagem da Melissa">
-            <img src="../img/pessoa2.jpg"  class="qm2" alt="aqui temos a imagem do José Guilherme">
-            <img src="../img/pessoa3.jpg" class="qm3" alt="aqui temos a imagem da Yasmin">
-            <img src="../img/pessoa4.jpg" class="qm4" alt="aqui temos a imagem do Gilberto">
+            <img src="melissa.jpg" class="qm1" alt="aqui temos a imagem da Melissa">
+            <img src="jose.jpg"  class="qm2" alt="aqui temos a imagem do José Guilherme">
+            <img src="yasmin.jpg" class="qm3" alt="aqui temos a imagem da Yasmin">
+            <img src="gilberto.jpg" class="qm4" alt="aqui temos a imagem do Gilberto">
             <a href="quemsomos.html"> <img src="quemsomos.webp.crdownload" alt="" class="qms"> </a>
         </article>
        
@@ -25,7 +28,7 @@
         <table>
             <thead>     
                 <tr>
-                    <th>NOME DO ALUNO</th>
+                    <th>ALUNO</th>
                     <th>O QUE FEZ NO PROJETO</th>
                     <th>RA</th>
                     <th>BIOGRAFIA</th>
@@ -33,36 +36,54 @@
             </thead>
             <tbody>
             <tr class="melissa">
-                <td>Melissa</td>
-                <td>Fez as seguintes páginas:</td>
-                <td>ra</td>
-                <td>melissa é uma menina e blá blá blá</td>
+           
+                <td>Melissa Raineri Godoy</td>
+                <td>
+                    <ul><li><a href ="cascão.php">Cascão</a></li>
+                  <li><a href ="cebolinha.php">Cebolinha</a></li>
+                  <li><a href ="magali.php">Magali</a></li>
+                  <li><a href ="monica.php">Mônica</a></li>
+                </ul>
+            </td>
+                    <td>265083221300-5</td>
+                <td>Melissa Raineri Godoy, integrante do grupo do projeto integrador que mora em Vargem Grande Paulista, tem 23 anos e está cursando
+                    o primeiro semestre em Sistemas para Internet na FATEC de São Roque.</td>
             </tr>
             <tr>
-                <td>José Guilherme</td>
-                <td>Fez as seguintes páginas</td>
-                <td>ra</td>
-                <td>José é um rapaz e blá blá blá</td>
+                <td>José Guilherme Guimarães Campos</td>
+                <td><ul>
+                <li><a href ="jogos.php">Jogos</a></li>
+                <li><a href ="historia-parte1.php">História - 1</a></li> 
+                <li><a href ="historia-parte2.php">História - 2</a></li>  
+                <li><a href ="brinquedos.php">Brinquedos</a></li>
+                </ul> 
+            </td>
+                <td>265083221300-6</td>
+                <td>José Guilherme Guimarães Campos, integrante do grupo do projeto integrador que mora em Mairinque, tem 25 anos e está cursando o primeiro semestre em para Internet na FATEC de São Roque.</td>
             </tr>
             <tr>
                 <td>Gilberto</td>
-                <td>Fez as seguintes paginas:</td>
-                <td>ra</td>
-                <td>Gilberto é um rapaz e blá blá blá</td>
+                <td><ul><li><a href ="colorir.php">Colorir</a></li>
+                    <li><a href ="index.php">Index</a></li>
+                    <li><a href ="gibis.php">Gibis</a></li>
+                    <li><a href ="atracoes.php">Atrações</a></li>
+                </ul></td>
+                <td>265083221300-8</td>
+                <td>Gilberto Giliarde Bueno Camargo, integrante do grupo do projeto integrador que mora em Mairinque , tem 18 anos e está cursando o primeiro semestre em Sistemas para Internet na FATEC de São Roque.</td>
             </tr>
             <tr>
                 <td>Yasmin</td>
-                <td>Fez as seguintes páginas:</td>
-                <td>ra</td>
-                <td>Yasmin é uma menina e blá blá blá</td></tr>
+                <td> <ul><li><a href ="desenho.php">Desenhos</a></li>
+                    <li><a href ="quem-somos.php">Quem somos</a></li>
+                    <li><a href ="quadrinhos.php">Quadrinhos</a></li>
+                    <li><a href ="faleconosco.php">Fale conosco</a></li></ul></td>
+                <td>2650832221302-4</td>
+                <td>	Yasmin do Nascimento, integrante do grupo do projeto integrador que mora em Vargem Grande Paulista, tem 18 anos e está cursando o primeiro semestre em Sistemas para Internet na FATEC de São Roque.</td></tr>
+
             </tbody>
-                <tfoot>
-                    <tr>
-                        <td>1234</td>
-                        <td>678</td>
-                        <td>0987</td>
-                        <td>45</td>
-                    </tr>
-                </tfoot>      
-        </table>
+               
+       
+ </table>
+ 
+ </body>
 </html>
