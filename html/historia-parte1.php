9ccf46f7d6822e5673d92d5d2b11caf6fe450a46
José Guilherme <jose.guilherme96@outlook.com.br>
2022-06-18 00:37:04 -0300

    Adcionando conteúdo do site

--- html/historia-parte1.php
+++ html/historia-parte1.php
@@ -15,19 +15,41 @@
                 <h1>História</h1>
                 <h2>Autor</h2>
             </hgroup>
-                <img src="https://via.placeholder.com/300x200" alt="Aqui tem uma foto do autor" class="foto-esquerda-texto">
-                <p class="texto-direito">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam velit metus, 
-                    condimentum non lacinia ac, fringilla eget ex. Aenean mattis ipsum at ligula condimentum iaculis. Mauris laoreet, metus eget pharetra luctus, ipsum lacus placerat leo, quis dapibus leo tellus ac leo. Sed pulvinar porttitor nisl vel sollicitudin. Fusce in semper nisl. Pellentesque at ornare diam, semper commodo 
-                    enim. Cras eu mollis metus, eu suscipit eros. Nulla vel dolor non odio semper porta nec nec nisi. Donec vitae egestas quam, vitae tristique magna. Sed enim dui, 
+                <img src="../img/mauricio.jpg" alt="Aqui tem uma foto do autor" class="foto-esquerda-texto">
+                <p class="texto-esquerdo">
+                Maurício de Souza é um cartunista que cria histórias da turma da Mônica, ele nasceu 
+                na cidade de santa Isabel, em 1935, Maurício vivia em uma família repleta pessoas 
+                talentosas, seus pais eram artistas, adoravam desenhar, pintar, fazer poesias e 
+                músicas, seu bisavô era violeiro e sua avó Dita adorava contar histórias para as 
+                crianças. Em sua casa tinha muitos livros, como toda criança curiosa, Maurício lia 
+                todos, até que um belo dia seu pai lhe deu de presente um gibi, e foi com ele que 
+                aprendeu a ler antes mesmo de entrar na escola. Maurício se inspirou em seus pais e 
+                começou a sonhar em ser como eles, queria se tornar desenhista quando crescesse, 
+                então começou desde cedo a se dedicar ao que tanto sonhava.
                 </p>
-                <img src="https://via.placeholder.com/300x200" alt="Aqui tem uma foto da turma da mônica" class="foto-direita-texto">
-                <p class="texto-esquerdo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam velit metus, 
-                    condimentum non lacinia ac, fringilla eget ex. Aenean mattis ipsum at ligula 
-                    condimentum iaculis. Mauris laoreet, metus eget pharetra luctus, ipsum lacus 
-                    placerat leo, quis dapibus leo tellus ac leo. Sed pulvinar porttitor nisl vel 
-                    sollicitudin. Fusce in semper nisl. Pellentesque at ornare diam, semper commodo 
-                    enim. Cras eu mollis metus, eu suscipit eros. Nulla vel dolor non odio semper 
-                    porta nec nec nisi. Donec vitae egestas quam, vitae tristique magna. Sed enim dui, 
+                <img src="../img/mauricio-mais-novo.jpg" alt="Aqui tem uma foto da turma da mônica" class="foto-direita-texto">
+                <p>
+                Não foi fácil para Maurício realizar seu sonho na época em que o Brasil não tinha 
+                espaço para o desenho, já que não fazia nenhum sucesso. Até que então Maurício 
+                resolveu apresentar a primeira pasta de desenhos a um jornal da época, porém o 
+                diretor de arte lhe disse “Desista, faça outra coisa na vida” e fechou sua pasta, 
+                Mauricio ficou muito triste por não ter conseguido convencer o diretor, foi embora e 
+                passou na frente de uma outra sala, um jornalista acabou o chamando, pois sentiu que 
+                Maurício estava muito triste e perguntou “Menino venha aqui o que que houve?”, 
+                Maurício contou o que havia ocorrido e entregou sua pasta de desenhos para o 
+                jornalista, então o jornalista disse “você desenha bem, mas apresentou mal, você não 
+                fez uma pasta bacana, chique...você cortou seus desenhos com tesoura e não pode, 
+                precisa enfeitar bem, apresentar bem”, assim o jornalista sugeriu que ele começasse a 
+                trabalhar no jornal e disse “entre no jornal para fazer qualquer coisa”, por mais que 
+                não fosse uma área que ele gostaria, como desenhista, poderia conhecer novas 
+                pessoas, e assim apresentar seus desenhos para elas.
+                Depois de algum tempo o jornalista chamou Maurício, pois tinha surgido uma vaga de 
+                revisor de texto, não era a área que ele queria, mas iria aprender muito. Ele aceitou a 
+                vaga e depois de duas semanas surgiu uma outra oportunidade para trabalhar como 
+                repórter policial, como disse Maurício “não era o que queria, mas era o caminho”, 
+                conseguiu a vaga pois tinha uma boa escrita e leitura, depois de algum tempo 
+                começou apresentar a sua história para o pessoal do jornal e os convenceu de 
+                publicar a sua primeira historinha do Bidu e Franguinha no jornal.
                 </p>
         </article>
         <footer class="final">
--- html/historia-parte2.php
+++ html/historia-parte2.php
@@ -15,23 +15,35 @@
                     <h2>História</h2>
                     <h1>Turma da mônica</h1>
                 </hgroup>
-                <img src="https://via.placeholder.com/300x200" alt="Aqui tem uma foto do autor" class="foto-esquerda-texto">
-                <p class="texto-direito">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam velit metus, 
-                    condimentum non lacinia ac, fringilla eget ex. Aenean mattis ipsum at ligula 
-                    condimentum iaculis. Mauris laoreet, metus eget pharetra luctus, ipsum lacus 
-                    placerat leo, quis dapibus leo tellus ac leo. Sed pulvinar porttitor nisl vel 
-                    sollicitudin. Fusce in semper nisl. Pellentesque at ornare diam, semper commodo 
-                    enim. Cras eu mollis metus, eu suscipit eros. Nulla vel dolor non odio semper 
-                    porta nec nec nisi. Donec vitae egestas quam, vitae tristique magna. Sed enim dui, 
+                <img src="../img/turma-da-monica.jpg" alt="Aqui tem uma foto do autor" class="foto-esquerda-texto">
+                <p class="texto-direito">
+                    A turma da Mônica é um conjunto de histórias em quadrinhos, que contam a história 
+                    de 4 personagens principais: Cebolinha, Mônica, Cascão e Magali.
+                    Mauricio, tinha um lápis que seu pai tinha dado para ele, que ele chamava de zé 
+                    rabisco, se tornou seu brinquedo predileto, usava seu lápis para rabiscar suas ideias, 
+                    criar seus personagens e escrever suas histórias, a 80 anos atrás foi onde tudo 
+                    começou.
+                    Maurício aprendeu a ler com jornaizinhos e quadrinhos, soletrava cada letra e depois 
+                    juntava para formar uma palavra, ficava muito feliz quando descobria uma palavra 
+                    nova, foi nestes quadrinhos que se inspirou e começou a se interessar para criar a 
+                    turma da Mônica, ele se colocava no lugar dos personagens e se achava em todos 
+                    eles, as vezes era flash Gordon, Jim das selvas, Batman, Popeye, todos eles eram 
+                    amigos de Maurício.
+                    Na sua época de escola “Maurício adorava ilustrar as lições que as professoras 
+                    passavam, cartazes de estudantes, caricaturas de colegas e professores”, foi onde 
+                    surgiram os primeiros personagens.
                 </p>
-                <img src="https://via.placeholder.com/300x200" alt="Aqui tem uma foto da turma da mônica" class="foto-direita-texto">
-                <p class="texto-esquerdo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam velit metus, 
-                    condimentum non lacinia ac, fringilla eget ex. Aenean mattis ipsum at ligula 
-                    condimentum iaculis. Mauris laoreet, metus eget pharetra luctus, ipsum lacus 
-                    placerat leo, quis dapibus leo tellus ac leo. Sed pulvinar porttitor nisl vel 
-                    sollicitudin. Fusce in semper nisl. Pellentesque at ornare diam, semper commodo 
-                    enim. Cras eu mollis metus, eu suscipit eros. Nulla vel dolor non odio semper 
-                    porta nec nec nisi. Donec vitae egestas quam, vitae tristique magna. Sed enim dui, 
+                <img src="../img/mauricio-jornal.jpg" alt="Aqui tem uma foto da turma da mônica" class="foto-direita-texto">
+                <p class="texto-esquerdo">
+                    Em 1959 Maurício era repórter policial e quando perguntavam para ele como ocorreu 
+                    um acontecimento, ele escrevia uma historinha de como aconteceu, então Maurício 
+                    resolveu criar mais e mais historinhas e ilustrá-las.
+                    Maurício lembrou da sua aventura de criança com seu cãozinho Cuica, e criou seus 
+                    primeiros personagens Bidu e Franjinha. Depois de fazer algumas tirinhas do Bidu, 
+                    Maurício publicou no jornal, depois de algum tempo deixou de ser repórter e passou a 
+                    se dedicar aos quadrinhos.
+                    Em 1960 e 1961 começaram a surgir os primeiros personagens: Cebolinha, Cascão, 
+                    Mônica e Magali....só que todos eles eram bem diferentes de como são hoje.
                 </p>
         </article>
         <footer class="final">
