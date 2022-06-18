18e99d3f5631cb2091da10ff93f3e982c1408f2c
Giliarde0 <106849333+Giliarde0@users.noreply.github.com>
2022-06-18 18:21:34 -0300

    "Arrumei umas certas coisas"

--- html/colorir.php
+++ html/colorir.php
@@ -14,27 +14,27 @@
         <article>
             <section>
                 <img src="../img/Colorir1.jpg" alt="Está imagem contém quatro personagens da turma da Monica perto de uma arvore de Natal sendo eles, o cascão, cebolinha, Monica e Magali em época de Natal com o personagem cebolinha com um presente na mão, a Monica enfeitando a arvore de Natal, Magali abrindo uma melancia de presente e o cascão zombando da Magali pela melancia de presente. ">
-                <h2> Colorir 1</h2>
+                <h2> Feliz Natal</h2>
             </section>
             <section>
                 <img src="../img/Colorir2.jpg" alt="Está imagem mostra os personagens Monica, Magali, cascão, cebolinha e Bidu descendo um escorregador com balões voando log atrás deles, todos estão felizes e saltando. "> 
-                <h2> Colorir 2</h2>
+                <h2> Dia de Parque </h2>
             </section>
             <section>
                 <img src="../img/Colorir3.jpg" alt="Nesta imagem podemos ver um balde de praia e uma pá de brinquedo, a Mônica, cebolinha e a Magali brincando na praia, e a Magali sendo enterrada pelos dois amigos, Mônica e cascão, e eles estão tomando sorvete enquanto a enterram, a Magali está irritada por conta disso. ">
-                <h2> Colorir 3</h2>
+                <h2> Diversão na Areia</h2>
             </section>
             <section>
                <img src="../img/Colorir4.jpg" alt="Nesta imagem é mostrado os personagens anjo, Magali, Mônica, cascão e cebolinha fazendo um desenho de uma arvore de Natal, e com roupas temáticas natalinas, preparando a arvore de Natal. ">
-               <h2> Colorir 4</h2>
+               <h2> Natal da Turma da Mônica </h2>
             </section>
             <section>   
                 <img src="../img/Colorir5.jpg" alt="Nesta imagem são apresentados o personagem cebolinha e o cascão em um campo de futebol, com roupas de futebol, meião, camisa, shorts e chuteira, correndo com uma bola no pé. ">
-                <h2> Colorir 5</h2>
+                <h2> Jogo valendo em 3,2,1 </h2>
             </section>
             <section>
                 <img src="../img/Colorir6.jpg" alt="Esta imagem mostra o cebolinha e a Mônica vestidos tematicamente na festa junina, e a Mônica está pegando no braço do cebolinha como se os dois fossem um par, estão sorrindo e muito felizes. ">
-                <h2> Colorir 6</h2>
+                <h2> Quadrilha da Mônica</h2>
             </section>  
         </article>      
     </body>
