6402b4a7ac7c2b2bd2761e6f40178ce0faf259c7
Giliarde0 <106849333+Giliarde0@users.noreply.github.com>
2022-06-18 16:42:18 -0300

    "comitei"

--- html/gibis.php
+++ html/gibis.php
@@ -13,7 +13,7 @@
         ?>
         <article>
             <section>
-                <img src="../img/Gibi1verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica">
+                <img src="../img/Gibi1verdadeiro.jpg" alt="Nesta imagem vemos a Magali com duas pizzas nas mãos, em um cenário mais retro, ela está junto do personagem Quinzinho.">
                 <h2> Magali</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                 Mauris volutpat felis in massa congue porta. Mauris posuere dui 
@@ -21,7 +21,7 @@
                 </p>
             </section>
             <section>
-                <img src="../img/Gibi2verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica">
+                <img src="../img/Gibi2verdadeiro.jpg" alt="Nesta imagem vemos os personagens Cebolinha e Magali  correndo de uma lhama, uma águia, e um gato encima de uma cobra, em um cenário da cor roxa.">
                 <h2>Cebolinha – O mistério da Alpaca Azul   </h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                 Mauris volutpat felis in massa congue porta. Mauris posuere dui 
@@ -29,7 +29,7 @@
                 </p>
             </section> 
             <section>
-                 <img src="../img/Gibi3verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica">
+                 <img src="../img/Gibi3verdadeiro.jpg" alt="Nesta imagem vemos o Cascão, o Cebolinha, e Xaveco observando a Magali e a Mônica caindo dentro de um buraco com Jacarés e Polvos prestes a atacar, e eles estão com um rosto assustado.">
                  <h2>Turma da Mônica Eliminados</h2>
                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                 Mauris volutpat felis in massa congue porta. Mauris posuere dui 
@@ -37,7 +37,7 @@
                 </p>
             </section>
             <section>
-                 <img src="../img/Gibi4verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica">
+                 <img src="../img/Gibi4verdadeiro.jpg" alt="Nesta imagem vemos os personagens Mônica e Magali encima de um carro com uma placa na sua frente escrita, Na Casa de Papel, fazendo uma referencia a serie da Netflix.">
                  <h2>Turma da Mônica na Casa de Papel</h2>
                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                 Mauris volutpat felis in massa congue porta. Mauris posuere dui 
@@ -45,7 +45,7 @@
                 </p>
             </section>
             <section>
-                <img src="../img/Gibi5verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica">
+                <img src="../img/Gibi5verdadeiro.jpg" alt="Nesta Imagem vemos os personagens Mônica e Cascão observando um pássaro saindo de um ovo no ninho do mesmo.">
                 <h2>Turma da Mônica – Como criar um Bebê Dragão</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                 Mauris volutpat felis in massa congue porta. Mauris posuere dui 
@@ -53,7 +53,7 @@
                 </p>
             </section>
             <section>
-                <img src="../img/Gibi6verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica">
+                <img src="../img/Gibi6verdadeiro.jpg" alt="Nesta imagem vemos os Personagens Mônica, Cascão e Cebolinha fugindo de um vilão do seriado que segura uma arma de choque.">
                 <h2>Turma da Mônica - O Maior Vilão de Todos os Tempos</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                 Mauris volutpat felis in massa congue porta. Mauris posuere dui 
@@ -61,7 +61,7 @@
                 </p>
             </section>
             <section>
-                <img src="../img/Gibi7verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica">
+                <img src="../img/Gibi7verdadeiro.jpg" alt="Nesta imagem vemos os personagens Mônica, Cebolinha, Magali e Cascão sendo cercados por robôs inteligentes.">
                 <h2>Turma da Mônica – Lixo Eletrônico</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                 Mauris volutpat felis in massa congue porta. Mauris posuere dui 
@@ -69,7 +69,7 @@
                 </p>
             </section>
             <section>
-                <img src="../img/Gibi8verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica">
+                <img src="../img/Gibi8verdadeiro.jpg" alt="Nesta imagem vemos o personagem Cascão,  Cebolinha e Marina em um campo de futebol, correndo com uma bola no pé.">
                 <h2>Cascão - O Sumiço do Troféu</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                 Mauris volutpat felis in massa congue porta. Mauris posuere dui 
@@ -77,7 +77,7 @@
                 </p>
             </section>
             <section>
-            <img src="../img/Gibi9verdadeiro.jpg" alt="Aqui temos uma Gibi da turma da monica ">
+            <img src="../img/Gibi9verdadeiro.jpg" alt="Nesta imagem vemos os personagens Cebolinha, Mônica e Bidu felizes brincando no quintal de uma casa.">
                 <h2>Cebolinha - Um Boné Muito Descolado</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                 Mauris volutpat felis in massa congue porta. Mauris posuere dui 
