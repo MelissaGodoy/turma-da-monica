e70a37fd54dc5ae51480a19ff43b3bc48877e144
MelissaGodoy <melissaraineri328@gmail.com>
2022-06-17 23:57:36 -0300

    validando atraçoes

--- html/atracoes.php
+++ html/atracoes.php
@@ -1,55 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link href="../css/default.css" rel="stylesheet">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="../css/default.css" rel="stylesheet">
         <link href="../css/menu.css" rel="stylesheet">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body class="bodyatrac">
-    <?php
-    include "menu.php";
-    ?>
-    <article class="articleatrac">
-        <section>
-            <img src="../img/parquedamonica.jpg" alt="parque da monica">
-            <p>O Parque da Mônica é um parque de diversão temático com os personagens criados por Maurício de Sousa</p>
-            <p>Endereço: Av. das Nações Unidas, 22540 - Jurubatuba, São Paulo - SP, 04795-000</p>
-            <li><a href="https://ecommerce.parquedamonica.com.br/">compre seu ingresso aqui</a></li>
-        </section>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body class="bodyatrac">
+        <?php
+        include "menu.php";
+        ?>
+        <article class="articleatrac">
+            <h1>Atrações</h1>
+            <section>
+                <h2>Parque da Mônica</h2>
+                <img src="../img/parquedamonica.jpg" alt="parque da monica">
+                <p>O Parque da Mônica é um parque de diversão temático com os personagens criados por Maurício de Sousa</p>
+                <p>Endereço: Av. das Nações Unidas, 22540 - Jurubatuba, São Paulo - SP, 04795-000</p>
+                <a href="https://ecommerce.parquedamonica.com.br/">compre seu ingresso aqui</a>
+            </section>
+            <section>
+                <h2>Espetáculo</h2>
+                <img src="../img/show.png" alt="show da turma da monica">
+                <p>Divertido espetáculo musical Turma da Mônica O Show, no Shopping Eldorado, em São Paulo. Na apresentação os personagens Mônica, Cebolinha, Cascão, Magali e Chico Bento encenam esquetes engraçadas, dinâmicas, lúdicas e interativas por meio de suas músicas mais conhecidas com arranjos novos e modernos.</p>
+                <p>Endereço:Av. Rebouças, 3970, loja 409 – São Paulo/SP.</p>
+                <a href="http://teatrodasartessp.com.br/ingressos/">compre seu ingresso aqui</a>
 
-        <section>
-            <img src="../img/show.png" alt="show da turma da monica">
-            <p>Divertido espetáculo musical Turma da Mônica O Show, no Shopping Eldorado, em São Paulo. Na apresentação os personagens Mônica, Cebolinha, Cascão, Magali e Chico Bento encenam esquetes engraçadas, dinâmicas, lúdicas e interativas por meio de suas músicas mais conhecidas com arranjos novos e modernos.</p>
-            <p>Endereço:Av. Rebouças, 3970, loja 409 – São Paulo/SP.</p>
-            <li><a href="http://teatrodasartessp.com.br/ingressos/">compre seu ingresso aqui</a></li>
-
-        </section>
-
-        <section>
-            <img src="../img/suites.jpg" alt="suites bourbon turma da monica">
-            <p>Venha aproveitar as suítes tematizadas com móveis exclusivos, enxoval especial, livros, gibis, bonecos e brinquedos, escultura 3D do personagem e outras surpresas</p>
-            <li><a href="https://www.bourbon.com.br/pacotes-e-promocoes/bourbon-atibaia-suite-tematica-monica">faça sua reserva já</a></li>
-        </section>
-
-        <section>
-        <img src="../img/seupequeno.jpg" alt="seu pequeno na turma da monica">
-        <p>crie um livro personalizado onde e o seu filho é o herói ao lado da Turma da Mônica. Crie online e receba em casa.</p>
-        <li><a href="https://www.dentrodahistoria.com.br/turma-da-monica/">clique aqui para criar</a></li> 
-    </section>  
-    </article>
-
-    <footer class="footeratrac">
-        <nav>
-            <ul class="footer3">
-                 <li><a href="monica.php">Mônica</a></li>
+            </section>
+            <section>
+                <h2>Suítes</h2>
+                <img src="../img/suites.jpg" alt="suites bourbon turma da monica">
+                <p>Venha aproveitar as suítes tematizadas com móveis exclusivos, enxoval especial, livros, gibis, bonecos e brinquedos, escultura 3D do personagem e outras surpresas</p>
+                <a href="https://www.bourbon.com.br/pacotes-e-promocoes/bourbon-atibaia-suite-tematica-monica">faça sua reserva já</a>
+            </section>
+            <section>
+                <h2>Seu pequeno na turma da Mônica</h2>
+                <img src="../img/seupequeno.jpg" alt="seu pequeno na turma da monica">
+                <p>crie um livro personalizado onde e o seu filho é o herói ao lado da Turma da Mônica. Crie online e receba em casa.</p>
+                <a href="https://www.dentrodahistoria.com.br/turma-da-monica/">clique aqui para criar</a> 
+            </section>  
+        </article>
+        <footer class="footeratrac">
+            <nav>
+                <ul class="footer3">
+                <li><a href="monica.php">Mônica</a></li>
                 <li><a href="cebolinha.php">Cebolinha</a></li> 
                 <li><a href="cascao.php ">Cascão</a></li>    
-            </ul>
-        </nav>
-    </footer>
-
-</body>
+                </ul>
+            </nav>
+        </footer>
+    </body>
 </html>
