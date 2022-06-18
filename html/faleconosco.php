2e87061bd6883342b9fe146a6187ef33748cb45b
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2022-06-18 11:29:21 -0300

    estilo.css

--- html/faleconosco.php
+++ html/faleconosco.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fale conosco</title>
-    <link rel="stylesheet" href="yteste.css"> 
+    <link rel="stylesheet" href="estilo.css"> 
 </head>
 <body class="fc">
     <?php
