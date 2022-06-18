d0f78b49514f44a357fcdcf70cae92a89fbf261a
MelissaGodoy <melissaraineri328@gmail.com>
2022-06-17 22:45:56 -0300

    validando pagina cebolinha

--- html/cebolinha.php
+++ html/cebolinha.php
@@ -1,47 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cebolinha</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/cebolinha.css">
-</head>
+<html lang="pt">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cebolinha</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/cebolinha.css">
+    </head>
 
-<body class="body">
+    <body class="body">
         <?php
             include "menu.php";
         ?>
-    <article class="article">
-
         <aside class="img-maior1">
             <img src="../img/Cebolinha-removebg-preview.png" alt="monica com samsao">   
         </aside>
-
         <aside class="img-menor1">
             <img src="../img/cebolinhamenor.png " alt="turma da monica">    
         </aside>
-
-        <section class="cebolinha">
-            <h1>Cebolinha</h1>
-        </section>
-
-        <section class="texto1">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam velit metus, condimentum non lacinia ac, fringilla eget ex. Aenean mattis ipsum at ligula
-                condimentum iaculis. Mauris laoreet, metus eget pharetra luctus, ipsum lacus placerat leo, quis dapibus leo tellus ac leo. Sed pulvinar porttitor nisl vel 
-                sollicitudin. Fusce in semper nisl. Pellentesque at ornare diam, semper commodo enim. Cras eu mollis metus, eu suscipit eros. Nulla vel dolor non odio semper
-                porta nec nec nisi. Donec vitae egestas quam, vitae tristique magna. Sed enim dui, cursus et justo iaculis, auctor venenatis dui. Mauris scelerisque consequat 
-                risus, eget condimentum leo tincidunt vel. Morbi semper ligula lobortis libero tempus, non gravida diam vestibulum. Vivamus tortor ex, consectetur ultrices 
-                consectetur eget, tincidunt quis urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam velit metus, condimentum non lacinia ac, fringilla eget ex. Aenean mattis ipsum at ligula
-                condimentum iaculis. Mauris laoreet, metus eget pharetra luctus, ipsum lacus placerat leo, quis dapibus leo tellus ac leo. Sed pulvinar porttitor nisl vel 
-                sollicitudin. Fusce in semper nisl. Pellentesque at ornare diam, semper commodo enim. Cras eu mollis metus, eu suscipit eros. Nulla vel dolor non odio semper
-                porta nec nec nisi. Donec vitae egestas quam, vitae tristique magna. Sed enim dui, cursus et justo iaculis, auctor venenatis dui. Mauris scelerisque consequat 
-                risus, eget condimentum leo tincidunt vel. Morbi semper ligula lobortis libero tempus, non gravida diam vestibulum.
-                sollicitudin.</p>
-        
-        </section>
-
+        <article class="texto1">
+            <h1 class="cebolinha">Cebolinha</h1>
+            <p>Cebolinha é menino de 7 anos de idade que tem apenas 5 fios de cabelo e se acha muito inteligente. 
+                Ele troca o “r” pelo “L” em suas falas e vive desenhando nos muros além de provocar sua amiga Mônica 
+                chamando-a de feia, baixinha e dentuça. Vive fazendo planos ‘infalíveis’ para derrotar a Mônica e 
+                tentando a todo momento roubar o seu coelhinho Sansão. Além disso faz planos para dar um banho em seu amigo cascão, que não gosta de água, e tenta fazer sua amiga Magali deixar de ser gulosa. 
+                No início era o personagem principal das histórias, porém logo passou o cargo para sua amiga Mônica. 
+                Foi inspirado em um dos amigos de infância do irmão de Maurício de Souza que também tinha os cabelos espetados e trocava o ‘r’ pelo ‘l’ em suas falas. 
+                Usa sempre camisa verde, short preto e sapatos marrons, sendo o único dos quatro principais personagens a usar sapato já que seus amigos Cascão, Mônica e Magali não têm o costume. 
+                Além disso é o único dos principais personagens que não é filho único pois possui uma irmã caçula. 
+                Tem um cachorro, chamado Floquinho, da raça “Lhasa Apso” que é extremamente peludo e verde, se assemelhando a um esfregão. 
+                Ao contrário de seu melhor amigo Cascão que não gosta de tomar banho, cebolinha é o personagem que mais aparece em contato com a água p>
+        </article>
         <footer>
             <nav>
                 <ul class="footer1"> 
@@ -52,6 +42,6 @@
             </nav>
         </footer>
       
-    </article>
-</body>
+    
+    </body>
 </html>
