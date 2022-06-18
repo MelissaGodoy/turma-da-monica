ac30814f38820745f91a78b25f279c0f394850ca
Giliarde0 <106849333+Giliarde0@users.noreply.github.com>
2022-06-18 17:09:56 -0300

    "alteração"

--- html/gibis.php
+++ html/gibis.php
@@ -15,73 +15,56 @@
             <section>
                 <img src="../img/Gibi1verdadeiro.jpg" alt="Nesta imagem vemos a Magali com duas pizzas nas mãos, em um cenário mais retro, ela está junto do personagem Quinzinho.">
                 <h2> Magali</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
-                 at tellus dictum, eu porttitor nunc pellentesque.
+                <p>Magali acha que a pizza devee ser com ketchup e Quinzinho, sem ketchup.
+                   Então, eles viajam no tempo através da máquina do tempo de 2 cientistas para verem se nos antepassados colocavam ketchup nas pizzas,
+                   mas eles causam muita confusão, fazendo pizza deixar de existir no tempo presente.
                 </p>
             </section>
             <section>
                 <img src="../img/Gibi2verdadeiro.jpg" alt="Nesta imagem vemos os personagens Cebolinha e Magali  correndo de uma lhama, uma águia, e um gato encima de uma cobra, em um cenário da cor roxa.">
                 <h2>Cebolinha – O mistério da Alpaca Azul   </h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
-                 at tellus dictum, eu porttitor nunc pellentesque.
+                <p>Alpacas são lindas e fofas, mas o que essa alpaca azul estaria fazendo no bairro do Limoeiro? Muitos mistérios rondam essa história!
                 </p>
             </section> 
             <section>
                  <img src="../img/Gibi3verdadeiro.jpg" alt="Nesta imagem vemos o Cascão, o Cebolinha, e Xaveco observando a Magali e a Mônica caindo dentro de um buraco com Jacarés e Polvos prestes a atacar, e eles estão com um rosto assustado.">
                  <h2>Turma da Mônica Eliminados</h2>
-                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
-                 at tellus dictum, eu porttitor nunc pellentesque.
+                 <p>A Turma fica eufórica quando surge a oportunidade de participar de um reality show no bairro do Limoeiro. Mas tudo pode ser diferente do que parece.
                 </p>
             </section>
             <section>
                  <img src="../img/Gibi4verdadeiro.jpg" alt="Nesta imagem vemos os personagens Mônica e Magali encima de um carro com uma placa na sua frente escrita, Na Casa de Papel, fazendo uma referencia a serie da Netflix.">
                  <h2>Turma da Mônica na Casa de Papel</h2>
-                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
-                 at tellus dictum, eu porttitor nunc pellentesque.
-                </p>
+                 <p>Parece que, finalmente, Cebolinha criou um plano que funciona. As meninas vão precisar de um plano ainda melhor para retomar o segredo contido em uma montanha de papéis.
             </section>
             <section>
                 <img src="../img/Gibi5verdadeiro.jpg" alt="Nesta Imagem vemos os personagens Mônica e Cascão observando um pássaro saindo de um ovo no ninho do mesmo.">
                 <h2>Turma da Mônica – Como criar um Bebê Dragão</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
-                 at tellus dictum, eu porttitor nunc pellentesque.
+                <p>Não seria ótimo ter um dragão de estimação? Mas quem sabe cuidar de um bebê dragão? É isso que Mônica e Cebolinha vão precisar descobrir nesta história!
                 </p>
             </section>
             <section>
                 <img src="../img/Gibi6verdadeiro.jpg" alt="Nesta imagem vemos os Personagens Mônica, Cascão e Cebolinha fugindo de um vilão do seriado que segura uma arma de choque.">
                 <h2>Turma da Mônica - O Maior Vilão de Todos os Tempos</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
-                 at tellus dictum, eu porttitor nunc pellentesque.
+                <p>Qual é o maior vilão da Turma da Mônica? Capitão Feio? Doutor Spam? Bruxa Viviane? Sem chance! Descubra nesta aventura quem é o maior vilão de todos os tempos... literalmente.
                 </p>
             </section>
             <section>
                 <img src="../img/Gibi7verdadeiro.jpg" alt="Nesta imagem vemos os personagens Mônica, Cebolinha, Magali e Cascão sendo cercados por robôs inteligentes.">
                 <h2>Turma da Mônica – Lixo Eletrônico</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
-                 at tellus dictum, eu porttitor nunc pellentesque.
+                <p>Mais uma vez, o terrível Doutor Spam tenta dominar o mundo real utilizando ameaças virtuais. Só que, desta vez, ele utiliza algo diferente e inesperado: muito lixo eletrônico. Será que a Turminha vai conseguir detê-lo?
                 </p>
             </section>
             <section>
                 <img src="../img/Gibi8verdadeiro.jpg" alt="Nesta imagem vemos o personagem Cascão,  Cebolinha e Marina em um campo de futebol, correndo com uma bola no pé.">
                 <h2>Cascão - O Sumiço do Troféu</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
-                 at tellus dictum, eu porttitor nunc pellentesque.
+                <p>O desespero toma conta do Cascão quando o tão sonhado troféu do campeonato do bairro desaparece. Será preciso uma boa investigação para descobrir o culpado.
                 </p>
             </section>
             <section>
             <img src="../img/Gibi9verdadeiro.jpg" alt="Nesta imagem vemos os personagens Cebolinha, Mônica e Bidu felizes brincando no quintal de uma casa.">
                 <h2>Cebolinha - Um Boné Muito Descolado</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Mauris volutpat felis in massa congue porta. Mauris posuere dui 
-                 at tellus dictum, eu porttitor nunc pellentesque.
+                <p>Cebolinha quer muito um boné novo e, para isso, vai precisar da ajuda da Mônica. Os dois vão aprender lições que nem imaginavam para realizar esse sonho.
                 </p>
             </section>  
         </article>
