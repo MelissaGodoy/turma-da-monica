f83623b2f4648751893b498cd32f035abe2db556
MelissaGodoy <melissaraineri328@gmail.com>
2022-06-19 18:26:25 -0300

    css atrações

--- html/atracoes.php
+++ html/atracoes.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link href="../css/default.css" rel="stylesheet">
+        <link href="../css/atracoes.css" rel="stylesheet">
         <link href="../css/menu.css" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
@@ -13,21 +13,24 @@
         include "menu.php";
         include "footer.php";
         ?>
-        <article class="articleatrac">
+        <article>
             <h1>Atrações</h1>
-            <section>
+            <section class="container">
                 <h2>Parque da Mônica</h2>
-                <img src="../img/parquedamonica.jpg" alt="parque da monica">
-                <p>O Parque da Mônica é um parque de diversão temático com os personagens criados por Maurício de Sousa</p>
-                <p>Endereço: Av. das Nações Unidas, 22540 - Jurubatuba, São Paulo - SP, 04795-000</p>
-                <a href="https://ecommerce.parquedamonica.com.br/">compre seu ingresso aqui</a>
+                <img src="../img/parquedamonica.jpg" alt="parque da monica" class="img">
+                <p class="pr">O Parque da Mônica é um parque de diversão temático com os personagens criados por Maurício de Sousa 
+                Endereço: Av. das Nações Unidas, 22540 - Jurubatuba, São Paulo - SP, 04795-000
+                </p>
+                
+                
             </section>
-            <section>
-                <h2>Espetáculo</h2>
-                <img src="../img/show.jpg" alt="show da turma da monica">
-                <p>Divertido espetáculo musical Turma da Mônica O Show, no Shopping Eldorado, em São Paulo. Na apresentação os personagens Mônica, Cebolinha, Cascão, Magali e Chico Bento encenam esquetes engraçadas, dinâmicas, lúdicas e interativas por meio de suas músicas mais conhecidas com arranjos novos e modernos.</p>
-                <p>Endereço:Av. Rebouças, 3970, loja 409 – São Paulo/SP.</p>
-                <a href="http://teatrodasartessp.com.br/ingressos/">compre seu ingresso aqui</a>
+            
+            <section class="container2">
+                <h2 class="sub">Espetáculo</h2>
+                <img src="../img/show.jpg" alt="show da turma da monica"class="img2">
+                <p class="pr2">Divertido espetáculo musical Turma da Mônica O Show, no Shopping Eldorado, em São Paulo. Na apresentação os personagens Mônica, Cebolinha, Cascão, Magali e Chico Bento encenam esquetes engraçadas, dinâmicas, lúdicas e interativas por meio de suas músicas mais conhecidas com arranjos novos e modernos.
+                   Endereço:Av. Rebouças, 3970, loja 409 – São Paulo/SP.</p>
+                
 
             </section>
             <section>
