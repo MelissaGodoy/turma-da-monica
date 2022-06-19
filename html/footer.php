d3feeed9228ff058693564d7a091434bbb5ec3a6
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2022-06-19 17:56:52 -0300

    teste

--- html/footer.php
+++ html/footer.php
@@ -4,5 +4,5 @@
     <img class="fot" src="wpp.jpg" alt="aqui temos as redes sociais da turma da MÔNICA">
     <img class="ff" src="face.jpg" alt="aqui temos as redes sociais da turma da MÔNICA">
     <img class="ffy" src="tw.jpg" alt="aqui temos as redes sociais da turma da MÔNICA">
-    <a href="quemsomos.html"><img class="qs" src="q-somos.jpg" alt="aqui temos a imagem dos integrantes da turma da MÔNICA"></a>       
+    <a href="quemsomos.php"><img class="qs" src="q-somos.jpg" alt="aqui temos a imagem dos integrantes da turma da MÔNICA"></a>       
 </footer>
